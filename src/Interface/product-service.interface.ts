4fa6c52abf144090f02256e99674bb4b6501877a
Alex <alexbaatista93@gmail.com>
2025-03-20 10:03:54 -0300

    fix

--- src/Interface/product-service.interface.ts
+++ src/Interface/product-service.interface.ts
@@ -2,11 +2,10 @@ import { CreateProductDto } from "src/dto/create-product.dto";
 import { UpdateDtoProduct } from "src/dto/update-product.dto";
 
 export interface IProductService {
-    listAllProducts(product_type?: string[]): Promise<any>; //Remover
     createProduct(createProductDto: CreateProductDto): Promise<{message: string}>;
     createProductPlanilha(createProductDto: CreateProductDto[]): Promise<any>;
     removeProduct(product_id: number): Promise<{message: string}>;
-    editProduct(product_id: number, updateDtoProduct: UpdateDtoProduct): Promise<{message: string}>; // Mudar nome
+    editProduct(product_id: number, updateDtoProduct: UpdateDtoProduct): Promise<{message: string}>;
     filterTypesOfProducts(): Promise<{message: string[]}>
     pagination( page: number, limit: number, type: string[] ): Promise<any>;
 }
